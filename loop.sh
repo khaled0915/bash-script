8af08ea56887963cd4465842a2c380e0e6e2bdf6
khaled0915 <khaledsaifullah50956@gmail.com>
2025-09-21 03:02:54 +0600

    practiced while loop and for loop

--- loop.sh
+++ loop.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
+#for loop
+# courses=('html' 'css' 'js' 'php' 'python')
+
+# for course in "${courses[@]}" ; do
+#     echo "course info $course"
+# done
+
+
+#while loop
+
+
 courses=('html' 'css' 'js' 'php' 'python')
 
-for course in "${courses[@]}" ; do
-    echo "course info $course"
+index=0 
+while [ $index -lt ${#courses[@]} ]; do
+    echo "course info ${courses[$index]}"
+    ((index++))
 done
