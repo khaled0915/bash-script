242fd6e9545a4bf491fbc5696935e09be41eade7
khaled0915 <khaledsaifullah50956@gmail.com>
2025-09-21 02:11:02 +0600

    create another file loop.sh

---
+++ loop.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+courses=('html' 'css' 'js' 'php' 'python')
+
+for course in "${courses[@]}" ; do
+    echo "course info $course"
+done
--- script.sh
+++ script.sh
@@ -1,3 +1,44 @@
 #!/bin/bash
 
 echo "hello world"
+
+name='khaled'
+
+age=24
+echo "my name is $name and my age is $age"
+
+echo "hello again and today is $(date)"
+
+
+#conditional statements
+
+# if [ $age -gt 20 ]; then
+#     echo "you are old enough"
+# else
+#     echo "you are too young"
+# fi
+
+# read -p "enter your name: " user_name
+# echo "welcome $user_name"
+
+# read -p "enter your email: " user_email
+# echo "your email is $user_email"
+# read -sp "enter your password: " user_password
+# echo "your password is $user_password"
+
+valid_email="khaled@gmail.com"
+valid_password="12345"
+
+read -p "enter your email: " user_email
+read -p "enter your password: " user_password
+
+#check and validate 
+
+if [[ $valid_email == $user_email && $valid_password == $user_password ]] ; then
+
+    echo "welcome $user_name"
+
+else
+    echo "invalid email or password"
+
+fi
